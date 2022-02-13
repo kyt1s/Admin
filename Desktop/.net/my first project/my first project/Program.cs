9f30c8b8ec9a8e642836fd6d9744e890d0c1226f
yash tiwary <ytiwary15@gmail.com>
2022-02-13 22:08:42 +0530

    project

---
+++ Desktop/.net/my first project/my first project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        Console.WriteLine("Enter n1");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        while(n1<=10)
+        {
+            Console.WriteLine(n1);
+            n1++;
+        }
+       
+    }
+    public static void Main(String[] args)
+    {
+        Class1 ob = new Class1();
+    }
+}
